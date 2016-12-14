0eb83fbd069b63a5504b155c25bd7f0e59e5f57b
Jacob van't Hoog <vantjac@gmail.com>
2016-12-13 21:52:58 -0800

    Start compiler

---
+++ story.js
@@ -0,0 +1,21 @@
+// WORK IN PROGRESS!
+
+// assumes that the expression has no errors
+function compileExpression(expression, language) {
+    
+}
+
+function checkExpression(expression, language) {
+    var errors = [ ]
+    
+    return errors;
+}
+
+
+
+function Error(message, type, index, length) {
+    this.message = message;
+    this.type = type;
+    this.index = index;
+    this.length = length;
+}
