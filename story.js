e3ce49d68918d3e83e9f37a2addc3ee17808402b
Jacob van't Hoog <vantjac@gmail.com>
2016-12-14 09:49:27 -0800

    Finish expression type detection

--- story.js
+++ story.js
@@ -1,5 +1,13 @@
 // WORK IN PROGRESS!
 
+
+if(typeof(String.prototype.trim) === "undefined") {
+    String.prototype.trim = function() {
+        return String(this).replace(/^\s+|\s+$/g, '');
+    };
+}
+
+
 // assumes that the expression has no errors
 function compileExpression(expression, language) {
     return _compileRecursive(expression, language);
@@ -13,19 +21,23 @@ function checkExpression(expression, language) {
 
 
 function _compileRecursive(expression, language) {
+    expression = expression.trim();
     var len = expression.length;
     if(len == 0)
         return null;
     // determine what type of expression this is...
-    var inComment = false, inString = false, stringStartChar,
+    var inComment = false, inString = false, stringStartChar, bracketDepth = 0,
         textInLine = false, textInPreviousLine = false;
     var multiline = false, numberLiteral = true, textLiteral = true,
         underscores = true, empty = true;
     for(var i = 0; i < len; i++) {
         var c = expression.charAt(i);
-        if(c == ' ' || c == '\t')
+        if(c == ' ' || c == '\t') {
+            underscores = false;
+            numberLiteral = false;
             continue;
-        if(c == '\n') {
+        }
+        if(c == '\n' && bracketDepth == 0) {
             inComment = false;
             textInPreviousLine = textInLine;
             textInLine = false;
@@ -44,7 +56,7 @@ function _compileRecursive(expression, language) {
             
             if(c != '_')
                 underscores = false;
-            if(isNaN(c))
+            if(isNaN(c) && c != '.' && c != '-')
                 numberLiteral = false;
             
             if(inString) {
@@ -58,12 +70,28 @@ function _compileRecursive(expression, language) {
                 } else {
                     textLiteral = false;
                 }
+                
+                if(c == '[')
+                    bracketDepth += 1;
+                if(c == ']')
+                    bracketDepth -= 1;
             }
         }
     }
     
-    if(empty)
+    if(empty) {
         return null;
+    } else if(multiline) {
+        console.log("multiline");
+    } else if(underscores) {
+        console.log("blank");
+    } else if(numberLiteral) {
+        console.log("number literal");
+    } else if(textLiteral) {
+        console.log("text literal");
+    } else {
+        console.log("command");
+    }
 }
 
 
@@ -73,4 +101,12 @@ function ScriptError(message, type, index, length) {
     this.index = index;
     this.length = length;
 }
+
+compileExpression("abc", null);
+compileExpression("1.23", null);
+compileExpression("\"abc\"", null);
+compileExpression("\"abc\" and \"abc\"", null);
+compileExpression("abc\n:abc\n", null);
+compileExpression("", null);
+compileExpression("_____", null);
 
